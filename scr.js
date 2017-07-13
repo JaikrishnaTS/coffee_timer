69dddef87d57c59ae16ee8c3ad446bd3c58ce4b2
Jaikrishna <t.s.jaikrishna@gmail.com>
2017-07-12 20:08:46 -0400

    Rewrite

--- scr.js
+++ scr.js
@@ -1,79 +1,85 @@
-var desc, tim, gram, start, clear, bloom, drip, grams, seconds, minutes, hours, secs, grainc, tt, t, cof, wat, tm, ts;
+var interval = null;
+var currentIncrement = 0;
+var initialised = false;
+var clicked = false;
+var coffee = 0, water = 0, tt = 0, curgram = 0, bloom = 30, drip = 30;
 
-window.onload = function() {
-  desc = document.getElementById('status');
-  tim = document.getElementById('time');
-  gram = document.getElementById('grams');
-  start = document.getElementById('start');
-  clear = document.getElementById('clear');
-  bloom = 30;
-  drip = 30;
-  grams = 0;
-  seconds = 0;
-  minutes = 0;
-  hours = 0;
-  secs = 0;
-  t = 0;
-  /* Start button */
-  start.onclick = startmes;
-  /* Clear button */
-  clear.onclick = function() {
-  clearTimeout(t);
-  initvars();
+$(".playpause").click(function(e) {
+  if (clicked) {
+    e.preventDefault();
+    return false;
   }
-};
 
-function add() {
-  secs++;
-  if (secs > bloom && secs <= (tt - drip))
-    grams += grainc;
-  if (secs == 1)
-    desc.textContent = "Wait for Blooming";
-  if (secs == bloom)
-    desc.textContent = "Add water";
-  if (secs == tt - drip)
-    desc.textContent = "Wait for Dripping";
-  if (++seconds >= 60) {
-    seconds = 0;
-    if (++minutes >= 60)
-      minutes = 0;
+  if (!initialised) {
+    reset();
+    initialised = true;
+    $(".playpause span").removeClass();
+    $(".playpause span").addClass("pause");
+    initialiseTimer();
+  } else {
+    reset();
   }
-  tim.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-  gram.textContent = parseInt(grams);
+});
 
-  if (secs < tt)
-    timer();
-  else
-    desc.textContent = "Done!";
-}
+$(".pause").click(function() {
+  reset();
+});
 
-function timer() {
-  t = setTimeout(add, 1000);
-}
-//timer();
-
-function startmes() {
-  t ? clearTimeout(t) : 0;
-  initvars();
-  cof = parseInt(document.getElementById('coffee').value);
-  wat = parseInt(document.getElementById('water').value);
-  tm = parseInt(document.getElementById('totalmin').value);
-  ts = parseInt(document.getElementById('totalsec').value);
-  grams = (cof * 2);
-  tt = (tm * 60) + ts;
-  grainc = (wat - grams) / (tt - bloom - drip);
+function initialiseTimer() {
+  interval = setInterval(function() {
+    var current = setCurrentIncrement();
+    updateStopwatch(current);
+  }, 1000);
+  coffee = $("#coffee").val();
+  water = $("#water").val();
+  tt = parseInt($("#minutes").val()*60) + parseInt($("#seconds").val());
+  $(".wat").hide();
+  $(".desc").text("Blooming").show();
+  curgram = coffee*2;
+  $("#coffee").val(curgram);
+  grainc = (water - curgram)/(tt - bloom - drip);
   console.log(grainc);
-  timer();
 }
 
+function updateStopwatch(increment) {
+  if(increment == bloom)
+    $(".desc").text("Add water");
+  if(increment == tt - drip)
+    $(".desc").text("Dripping");
+  if(increment >= tt) {
+    clearInterval(interval);
+    initialized = false;
+    $(".desc").text("Done!");
+  }
+  if(increment > bloom && increment <= tt - drip) {
+    curgram += grainc;
+    $("#coffee").val(parseInt(curgram));
+  }
+  var minutes = Math.floor(increment / 60);
+  var seconds = increment - (minutes * 60);
+  if(minutes > 99)
+    reset();
 
-function initvars() {
-  desc.textContent = "Starting"
-  tim.textContent = "00:00";
-  gram.textContent = "0";
-  seconds = 0;
-  minutes = 0;
-  grams = 0;
-  secs = 0;
+  $("#minutes").val(minutes < 10 ? ("0" + minutes.toString()) : minutes.toString());
+  $("#seconds").val(seconds < 10 ? ("0" + seconds.toString()) : seconds.toString());
+  
 }
 
+function setCurrentIncrement() {
+  currentIncrement += 1;
+  return currentIncrement;
+}
+
+function reset() {
+  currentIncrement = 0;
+  initialised = false;
+  clearInterval(interval);
+  $("#coffee").val("22");
+  $("#minutes").val("03");
+  $("#seconds").val("00");
+  $("#water").val("210");
+  $(".playpause span").removeClass();
+  $(".playpause span").addClass("play");
+  $(".desc").text("Start").hide();
+  $(".wat").show();
+}
