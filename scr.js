677b9921dc581f1abc62179611c17125d25838a0
Jaikrishna <t.s.jaikrishna@gmail.com>
2017-07-13 17:57:34 -0400

    Fix reset bug; add local storage support

--- scr.js
+++ scr.js
@@ -1,11 +1,13 @@
 var interval = null;
 var currentIncrement = 0;
 var initialised = false;
+var completed = false;
 var coffee = 0, water = 0, tt = 0, curgram = 0, bloom = 30, drip = 30;
 
 function ppclick() {
-  if (!initialised) {
-    reset();
+  if (completed) {
+      reset();
+  } else if (!initialised) {
     initialised = true;
     $(".playpause span").removeClass();
     $(".playpause span").addClass("pause");
@@ -23,7 +25,13 @@ function initialiseTimer() {
   }, 1000);
   coffee = $("#coffee").val();
   water = $("#water").val();
-  tt = parseInt($("#minutes").val()*60) + parseInt($("#seconds").val());
+  mins = parseInt($("#minutes").val());
+  secs = parseInt($("#seconds").val());
+  localStorage.coffee = coffee;
+  localStorage.water = water;
+  localStorage.mins = mins;
+  localStorage.secs = secs;
+  tt = mins*60 + secs;
   $(".wat").hide();
   $(".desc").text("Blooming").show();
   curgram = coffee*2;
@@ -39,12 +47,12 @@ function updateStopwatch(increment) {
     $(".desc").text("Dripping");
   if(increment >= tt) {
     clearInterval(interval);
-    initialized = false;
+    completed = true;
     $(".desc").text("Done!");
   }
   if(increment > bloom && increment <= tt - drip) {
     curgram += grainc;
-    $("#coffee").val(parseInt(curgram));
+    $("#coffee").val(Math.round(curgram));
   }
   var minutes = Math.floor(increment / 60);
   var seconds = increment - (minutes * 60);
@@ -53,7 +61,6 @@ function updateStopwatch(increment) {
 
   $("#minutes").val(minutes < 10 ? ("0" + minutes.toString()) : minutes.toString());
   $("#seconds").val(seconds < 10 ? ("0" + seconds.toString()) : seconds.toString());
-  
 }
 
 function setCurrentIncrement() {
@@ -64,14 +71,21 @@ function setCurrentIncrement() {
 function reset() {
   clearInterval(interval);
   currentIncrement = 0;
-  $("#coffee").val("22");
-  $("#minutes").val("02");
-  $("#seconds").val("00");
-  $("#water").val("210");
+  if(localStorage.coffee == null) {
+      localStorage.coffee = 15;
+      localStorage.water = 210;
+      localStorage.mins = 2;
+      localStorage.secs = 40;
+  }
+  $("#coffee").val(localStorage.coffee);
+  $("#minutes").val(localStorage.mins);
+  $("#seconds").val(localStorage.secs);
+  $("#water").val(localStorage.water);
   $(".playpause span").removeClass();
   $(".playpause span").addClass("play");
   //$(".playpause").click(ppclick);
   $(".desc").text("Start").hide();
   $(".wat").show();
   initialised = false;
+  completed = false;
 }
